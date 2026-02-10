adf8f9b573c22b087939ccc47b6b2d60437b1560
MariusFiat <mariusfiat07@gmail.com>
2026-02-10 20:18:26 +0200

    #5 Read from the RPI5 the response gived by car plate detection stage.

--- RPI_Pico/src/board_config.h
+++ RPI_Pico/src/board_config.h
@@ -2,11 +2,19 @@
 #define __BOARD_CONFIG_H
 
 #define USE_PICO_WH 1 //Board type, pico wifi or not.
-#define TASK_DELAY 50
+#define TASK_DELAY 500
 
 /* Pins declarations */
 #define ENTRY_SENSOR 0  /* IR sensor for vehicle detection at the parking entry. */
 #define EXIT_SENSOR 1 /* IR sensor for vehicle detection at the parking exit. */
+
+/* Messages lengths and format. 
+    ENAXXX = Entry allowed slot number XXX
+    END000 = Entry denied
+    EXA000 = Exit allowed 
+    EXD000 = Exit denied (Maybe you forgot to pay your slot.)
+*/
+#define COMMAND_LENGTH 6
 
 /*Board hardware init functions*/
 void init_board();
--- RPI_Pico/src/uart_handler.c
+++ RPI_Pico/src/uart_handler.c
@@ -8,27 +8,49 @@
 #include "shared_resources.h"
 #include "uart_handler.h"
 
+static void checkEntryRequest();
+static void checkExitRequest();
+static bool read_user_data(char*);
+
+static bool sensorStateReceived = false;
+static bool lastEntryRead = false;
+static int received_Response;
+static bool readEntryResponseDone = false;
+
+static char command_Entry[COMMAND_LENGTH + 1];
+static int i = 0;
+
 void uart_handler(void* pvParams){
     (void) pvParams;
 
-    bool sensorStateReceived = false;
-    static bool lastEntryRead = false;
-
-    int received_Response;
-
     while(1){
-        received_Response = getchar_timeout_us(0);
+        //printf("%s\n", "The uart task is running!");
+        checkEntryRequest();
 
-        if(lastEntryRead == true && received_Response != PICO_ERROR_TIMEOUT){
-            while(getchar_timeout_us(0) != PICO_ERROR_TIMEOUT); /* Clear the buffer. Delete the unused characters like '\n'*/
+        vTaskDelay(pdMS_TO_TICKS(TASK_DELAY));
+    }
+}
+
+static void checkEntryRequest(){
+
+        if(lastEntryRead == true){
             /* Send signal to the python scrip */
 
             /* If the response is received, than, open/not the barrier*/
+            readEntryResponseDone = read_user_data(command_Entry);
             
-            lastEntryRead = false;
-            xSemaphoreGive(xSemaphore_Entry_Res); /* Wake-up the detect entry task. */
+            if(readEntryResponseDone){
+                if(strstr(command_Entry, "EA") != NULL){
+                        
+                    printf("Entry access allowed!\n");
+                }else{
+                    printf("Entry access denied!\n");
+                }
 
-            vTaskDelay(pdMS_TO_TICKS(100));
+                lastEntryRead = false;
+                xSemaphoreGive(xSemaphore_Entry_Res); /* Wake-up the detect entry task. */
+                vTaskDelay(pdMS_TO_TICKS(100));
+            }
         }
         else if(lastEntryRead == false){ /* If there was not detected any obstacle, than run normally. */
             
@@ -43,7 +65,31 @@ void uart_handler(void* pvParams){
                 xSemaphoreTake(xSemaphore_Entry_Res, 0); /* Call the semaphore to block the detect_entry task. Now, when the detect_entry task will call SemaporeTake, it will be blocked. */
             }
         }
+}
 
-        vTaskDelay(pdMS_TO_TICKS(TASK_DELAY));
+static bool read_user_data(char *out_buffer) {
+    static char internal_buffer[COMMAND_LENGTH + 1];
+    static int current_idx = 0;
+
+    int c = getchar_timeout_us(0);
+
+    if (c != PICO_ERROR_TIMEOUT) {
+        if (c >= 32 && c <= 126) { 
+            internal_buffer[current_idx++] = (char)c;
+        }
+
+        if (current_idx == COMMAND_LENGTH) {
+            internal_buffer[COMMAND_LENGTH] = '\0';
+            
+            for(int i = 0; i <= COMMAND_LENGTH; i++) {
+                out_buffer[i] = internal_buffer[i];
+            }
+
+            current_idx = 0;
+            printf("\n[System] RPI5 send: %s\n", out_buffer);
+            return true; 
+        }
     }
+
+    return false;
 }
