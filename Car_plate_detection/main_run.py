18daeae60b3bf265baca1304506de8158805e18d
MariusFiat <mariusfiat07@gmail.com>
2026-02-17 15:03:22 +0200

    #8 Store the commands in a queue that handle the serial input from the RPI_Pico and than execute the requests in a serial mode because I have just a camera

--- Car_plate_detection/main_run.py
+++ Car_plate_detection/main_run.py
@@ -9,52 +9,65 @@ import serial
 from CameraWrapper import CameraWrapper
 from yolo4 import plateRecognition
 from checkplates import check_license_plate
+from queue import Queue
 
 def log(msg):
     print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {msg}", flush=True)
 
-def main():
-    port_virtual = '/dev/ttyACM0' # This is the pico serial port.
-    ser = serial.Serial(port_virtual, 115200, timeout = 1)
+serialQueue = Queue(maxsize = 2)
+port_virtual = '/dev/ttyACM0' # This is the pico serial port.
+ser = serial.Serial(port_virtual, 115200, timeout = 1)
+cap = CameraWrapper()
 
-    print("\nStarting parking recognition service...")
-    cap = CameraWrapper()
-    
+def serial_handler():
+    while True:
+        line_raw = ser.readline()
+        line = line_raw.decode('utf-8')
 
-    
-    try:    
+        if not line:
+            continue
         
-        while True:
-            line_raw = ser.readline()
-                
-            line = line_raw.decode('utf-8').strip()
+        print(f"Command received : {line}")
+        print(f"Line[0] : {line[0]}")
+        
+        if serialQueue.full() == False:
+            if len(line) == 2:
+                serialQueue.put(line[0])
+                serialQueue.put(line[1])
+            else:
+                serialQueue.put(line[0])
             
-            if not line:
-                continue
+        #Consume the commands
+        while serialQueue.empty() == False:
+            command = serialQueue.get()
+            print(f"Command under execution: {command}")
+            detectSide(command)
 
-            print(f"Comanda primita: {line}")
+        time.sleep(0.01)
 
-            if line == "A":
-                print("[System] Cerere Intrare (A)")
-                plateNumber = plateRecognition(cap, "left")
-                result = check_license_plate(plateNumber)
-                
-                ser.write("ENA000\n".encode('utf-8'))
-                ser.flush()
-                print("Trimis catre Pico: ENA000")
 
-            elif line == "B":
-                print("[System] Cerere Iesire (B)")
-                plateNumber = plateRecognition(cap, "right")
-                result = check_license_plate(plateNumber)
+def detectSide(side):
+    if side == 'A':
+        print("[System] Entry request (A)")
+        plateNumber = plateRecognition(cap, "left")
+        result = check_license_plate(plateNumber)
                 
-                ser.write("EXA000\n".encode('utf-8'))
-                ser.flush()
-                print("Trimis catre Pico: EXA000")
-            
-            time.sleep(0.01)
+        ser.write("ENA000\n".encode('utf-8'))
+        ser.flush()
+        print("Sent catre Pico: ENA000")
+    else:
+        print("[System] Exit request (B)")
+        plateNumber = plateRecognition(cap, "right")
+        result = check_license_plate(plateNumber)
                 
-    except KeyboardInterrupt:
-        print("\nScript stopped by user.")
+        ser.write("EXA000\n".encode('utf-8'))
+        ser.flush()
+        print("Sent to Pico: EXA000")
+
+
+def main():
+    print("\nStarting parking recognition service...")
+
+    serial_handler()
     
 main()
