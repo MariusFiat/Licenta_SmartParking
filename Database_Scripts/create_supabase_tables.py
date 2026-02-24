4f78ee997c1af2591d51d0fdabbaaf2af4d4eac3
MariusFiat <mariusfiat07@gmail.com>
2026-02-24 19:14:13 +0200

    #12 Trigger fix, add metadata

--- Database_Scripts/create_supabase_tables.py
+++ Database_Scripts/create_supabase_tables.py
@@ -100,19 +100,25 @@ def insert_reserved_unknown_user_id(conn, cur):
 
 def setup_auth_trigger(conn, cur):
     trigger_sql = """
+    DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
+
     CREATE OR REPLACE FUNCTION public.handle_new_user()
     RETURNS trigger AS $$
     BEGIN
-      INSERT INTO public.user_details (id, first_name, last_name, subscription_type)
-      VALUES (new.id, '', '', 'STANDARD');
-      RETURN new;
+    INSERT INTO public.user_details (id, first_name, last_name, subscription_type)
+    VALUES (
+        new.id, 
+        COALESCE(new.raw_user_meta_data->>'first_name', 'NoFirstName'), 
+        COALESCE(new.raw_user_meta_data->>'last_name', 'NoLastName'), 
+        'STANDARD'
+    );
+    RETURN new;
     END;
     $$ LANGUAGE plpgsql SECURITY DEFINER;
 
-    DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
     CREATE TRIGGER on_auth_user_created
-      AFTER INSERT ON auth.users
-      FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
+    AFTER INSERT ON auth.users
+    FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
     """
     cur.execute(trigger_sql)
     conn.commit()
