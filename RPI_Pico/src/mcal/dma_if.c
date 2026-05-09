67f591bd8084e064639eade936a91726b5ed0fe4
Marius Fiat <mariusfiat07@gmail.com>
2026-05-09 22:55:09 +0300

    #16 - Add the mcal directory + dma interfaces

---
+++ RPI_Pico/src/mcal/dma_if.c
@@ -0,0 +1,103 @@
+/*
+    * dma_if.c
+    *
+    *  Created on: 8 May 2024
+    *  MCU Abstraction Layer for Direct Memory Access (DMA) controller of the Raspberry Pi Pico. [..]
+    * [..] This module provides functions to initialize and manage DMA channels for efficient data transfer 
+    * [..] between peripherals and memory without CPU intervention. 
+    * In this case, the ADC module will use the DMA to save it's converstion results.
+*/
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+#include "board_config.h"
+#include "dma_if.h"
+
+uint8_t x_dma_init(uint8_t channel_number){
+    uint8_t retVal = RET_NOK;
+
+    /* Set all registers to 0. */
+    if(channel_number < DMA_NUM_OF_CHANNELS){
+        xHW_REG32(x_DMA_CH0_READ_ADDR + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) = 0;
+        xHW_REG32(x_DMA_CH0_WRITE_ADDR + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) = 0;
+        xHW_REG32(x_DMA_CH0_TRANS_COUNT + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) = 0;
+
+        retVal = RET_OK;
+    } else{
+        /* Invalid channel number*/
+    }
+
+    return retVal;
+}
+
+uint8_t x_dma_set_read_addr(uint32_t addr, uint8_t channel_number){
+    uint8_t retVal = RET_NOK;
+
+    if(channel_number < DMA_NUM_OF_CHANNELS){
+        /* Set the source address for DMA channel with number "channel_number"*/
+        xHW_REG32(x_DMA_CH0_READ_ADDR + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) = addr;
+        
+        retVal = RET_OK;
+    } else {
+        /* Invalid channel number*/
+    }
+
+    return retVal;    
+}
+
+uint8_t x_dma_set_write_addr(uint32_t addr, uint8_t channel_number){
+    uint8_t retVal = RET_NOK;
+
+    if(channel_number < DMA_NUM_OF_CHANNELS){
+        /* Set the destination address for DMA channel with number "channel_number" */
+        xHW_REG32(x_DMA_CH0_WRITE_ADDR + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) = addr;
+
+        retVal = RET_OK;
+    }
+    
+    return retVal;
+}
+
+uint8_t x_dma_set_trans_count_and_data_size(uint32_t trans_count, uint8_t data_size, uint8_t channel_number){
+    uint8_t retVal = RET_NOK;
+
+    if((channel_number < DMA_NUM_OF_CHANNELS) && ((data_size >= DMA_DATA_SIZE_1B) || (data_size <= DMA_DATA_SIZE_4B))){
+        /* Set the number of transfers for DMA channel with number "channel_number"*/
+        xHW_REG32(x_DMA_CH0_TRANS_COUNT + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) = trans_count;
+
+        /* Set the data size for this transfer */
+        xHW_REG32(x_DMA_CH0_CTRL_REG + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) |= (data_size << x_DMA_CH0_CTRL_DATA_SIZE_OFFSET);
+
+        retVal = RET_OK;
+    }
+    
+    return retVal;
+}
+
+uint8_t x_dma_set_trigger_source(uint32_t trig_source, uint8_t channel_number){
+    uint8_t retVal = RET_NOK;
+
+    if(channel_number < DMA_NUM_OF_CHANNELS){
+        /* Set the DMA transfer trigger source */
+        xHW_REG32(x_DMA_CH0_CTRL_REG + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) |= (trig_source << x_DMA_CH0_CTRL_TREQ_OFFSET);
+
+        retVal = RET_OK;
+    }
+
+    return retVal;
+}
+
+uint8_t x_dma_enable_channel(uint8_t channel_number){
+    uint8_t retVal = RET_NOK;
+
+    if(channel_number < DMA_NUM_OF_CHANNELS){
+        /* Set the enable bit */
+        xHW_REG32(x_DMA_CH0_CTRL_REG + (channel_number * x_DMA_CHANNEL_REGS_OFFSET)) |= (1 << x_DMA_CH0_CTRL_EN_BIT);
+
+        retVal = RET_OK;
+    }
+
+    return retVal;
+}
---
+++ RPI_Pico/src/mcal/dma_if.h
@@ -0,0 +1,35 @@
+#ifndef __DMA_H
+#define __DMA_H
+
+//* DMA Details */
+#define DMA_NUM_OF_CHANNELS                     12
+#define DMA_DATA_SIZE_1B                        0x0
+#define DMA_DATA_SIZE_2B                        0x1
+#define DMA_DATA_SIZE_4B                        0x2
+
+//* List of registers */
+#define x_DMA_BASE_ADDRESS                      0x50000000                          /* The base address for DMA pheripheral*/
+#define x_DMA_CHANNEL_REGS_OFFSET               0x040      
+#define x_DMA_CH0_READ_ADDR                     (x_DMA_BASE_ADDRESS + 0x000)        /* The read address register for DMA channel 0 */
+#define x_DMA_CH0_WRITE_ADDR                    (x_DMA_BASE_ADDRESS + 0x004)        /* The write address register for DMA channel 0 */
+#define x_DMA_CH0_TRANS_COUNT                   (x_DMA_BASE_ADDRESS + 0x008)        /* The register that contains the number of transfers for each DMA transfer sequence */
+#define x_DMA_CH0_CTRL_REG                      (x_DMA_BASE_ADDRESS + 0x00C)        /* Control and status register for DMA channel 0 */
+
+#define x_DMA_CH0_CTRL_DATA_SIZE_OFFSET         2                                   /* DATA_SIZE offset in CTRL reg*/
+#define x_DMA_CH0_CTRL_EN_BIT                   0                                   /* Enable bit offset in CTRL reg. Enables the channel that will start a new trasnfer when a TREQ is enabled. */
+#define x_DMA_CH0_CTRL_TREQ_OFFSET              15                                  /* TREQ offset in CTRL reg. This field sets the REQ source for a DMA transfer */
+
+#define x_DMA_ADC_DREQ                          0x24                                /* TREQ value for ADC DMA request*/
+
+//This DMA module contains 12 independent channels. Each chanel has 64b reserved for it's registers
+//[..] and bcs of that, I can configure every channel with the same function + channel offset.
+
+//* Public functions */
+uint8_t x_dma_init(uint8_t channel_number);
+uint8_t x_dma_set_read_addr(uint32_t addr, uint8_t channel_number);
+uint8_t x_dma_set_write_addr(uint32_t addr, uint8_t channel_number);
+uint8_t x_dma_set_trans_count_and_data_size(uint32_t trans_count, uint8_t data_size, uint8_t channel_number);
+uint8_t x_dma_set_trigger_source(uint32_t trig_source, uint8_t channel_number);
+uint8_t x_dma_enable_channel(uint8_t channel_number);
+
+#endif /* __DMA_H */
