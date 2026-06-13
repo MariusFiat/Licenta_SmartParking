25af667cb5581d08048d8385b195f1558d0f932e
Marius Fiat <mariusfiat07@gmail.com>
2026-06-13 21:48:27 +0300

    #19 - PWM integration, still in progress

--- RPI_Pico/src/mcal/pwm_if.c
+++ RPI_Pico/src/mcal/pwm_if.c
@@ -11,3 +11,25 @@
 #include "board_config.h"
 
 #include "pwm_if.h"
+
+void x_pwm_init(const uint8_t channel)
+{
+    /* Clear the registers for this slice*/
+    x_HWREG32(x_PWM_CSR + (channel * x_PWM_CHANNEL_x_OFFSET)) = 0;
+    x_HWREG32(x_PWM_DIV + (channel * x_PWM_CHANNEL_x_OFFSET)) = 1;
+    x_HWREG32(x_PWM_CTR + (channel * x_PWM_CHANNEL_x_OFFSET)) = 0;
+    x_HWREG32(x_PWM_CCR + (channel * x_PWM_CHANNEL_x_OFFSET)) = 0;
+    x_HWREG32(x_PWM_TOP + (channel * x_PWM_CHANNEL_x_OFFSET)) = 0;
+}
+
+void x_pwm_set_div(const uint8_t channel, const uint16_t div_int, const uint8_t div_frac)
+{
+    uint32_t div_value = (div_int << x_PWM_DIV_INT_OFFSET) | (div_frac << x_PWM_DIV_FRAC_OFFSET);
+    x_HWREG32(x_PWM_DIV + (channel * x_PWM_CHANNEL_x_OFFSET)) = div_value;
+}
+
+void x_pwm_set_wrap(const uint8_t channel, const uint16_t wrap)
+{
+    x_HWREG32(x_PWM_TOP + (channel * x_PWM_CHANNEL_x_OFFSET)) = wrap;
+}
+
--- RPI_Pico/src/mcal/pwm_if.h
+++ RPI_Pico/src/mcal/pwm_if.h
@@ -1,6 +1,25 @@
 #ifndef PWM_IF_H
 #define PWM_IF_H
 
+/* PWM registers */
 
+#define x_PWM_BASE                      0x40050000
+#define x_PWM_CSR                       (x_PWM_BASE + 0x00)
+#define x_PWM_DIV                       (x_PWM_BASE + 0x04)
+#define x_PWM_CTR                       (x_PWM_BASE + 0x08)
+#define x_PWM_CC                        (x_PWM_BASE + 0x0C)
+#define x_PWM_TOP                       (x_PWM_BASE + 0x10)
+#define x_PWM_CHANNEL_x_OFFSET          0x14
+
+/* PWM special bits */
+
+#define x_PWM_CSR_EN                    0x01
+#define x_PWM_PHASE_CORRECT             0x02
+#define x_PWM_DIV_INT_OFFSET            0x04
+#define x_PWM_DIV_FRAC_OFFSET           0x00
+#define x_PWM_CC_A_OFFSET               0x00
+#define x_PWM_CC_B_OFFSET               0x0F
+
+/* PWM interfaces */
 
 #endif /* PWM_IF_H */
