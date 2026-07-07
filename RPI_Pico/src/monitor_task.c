4849cfa984b45c6985530fc3e1fee73e11c25d61
Marius Fiat <mariusfiat07@gmail.com>
2026-07-07 14:11:23 +0300

    24 Update monitor task

--- RPI_Pico/src/monitor_task.c
+++ RPI_Pico/src/monitor_task.c
@@ -13,19 +13,33 @@ void monitor_task(void *pvParameters) {
         printf("          ANALIZA DE PERFORMANTA FREERTOS         \n");
         printf("==================================================\n");
 
-        printf("\n--- Stack consumption (Remaining RAM) ---\n");
-        printf("Task name\tState\tPriority\tRemaining\tNumber\n");
-        vTaskList(stats_buffer);
-        printf("%s\n", stats_buffer);
+        /* 1. Statistics about Heap Memory */
+        printf("\n--- Heap Memory Statistics ---\n");
+        printf("Current available Heap memory: %u bytes\n", xPortGetFreeHeapSize());
+        
+        /* Note: xPortGetMinimumEverFreeHeapSize works with heap_4.c and heap_5.c */
+        printf("Minimum ever available Heap memory:   %u bytes\n", xPortGetMinimumEverFreeHeapSize());
+        
+        
+        printf("High Water Mark (Monitor Task):  %lu words\n", uxTaskGetStackHighWaterMark(NULL));
 
-        printf("\n--- CPU Load (Execution Time) ---\n");
-        printf("Task name\tAbsolute Time\tPercentage (%%)\n");
+        /* 2. Lista task-urilor și consumul de stivă (High Water Mark global) */
+        printf("\n--- Task Status and Stack Usage ---\n");
+        printf("Task Name\tState\tPriority\tHigh Water\tCount\n");
+        
+        vTaskList(stats_buffer);
+        printf("%s", stats_buffer);
+
+        /* 3. Cpu Load */
+        printf("\n--- Cpu Load (Run Time Stats) ---\n");
+        printf("Task Name\tAbsolute Time\tPercentage (%%)\n");
+        
         vTaskGetRunTimeStats(stats_buffer);
-        printf("%s\n", stats_buffer);
+        printf("%s", stats_buffer);
         
         printf("==================================================\n");
 
-        /* Execute every 5 seconds */
+        /* Executa o data la 5 secunde */
         vTaskDelay(pdMS_TO_TICKS(5000)); 
     }
 }
